ed14a9c4d7ebd8280eab930f40816a3153d33521
Gustavo Marengo <56846264+gumarengo@users.noreply.github.com>
2020-03-18 22:33:42 -0300

    Add files via upload

---
+++ Atividade1/Volume/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Volume
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double verify1, verify2;
+
+            if ((double.TryParse(textBox1.Text, out verify1)) && (double.TryParse(textBox2.Text, out verify2)))
+            {
+                double volume = Math.Pow(verify1, 2) * Math.PI * verify2;
+                textBox3.Text = volume.ToString("N2");
+            }
+            else
+                MessageBox.Show("Dados inválidos");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+        }
+        }
+    }
+
