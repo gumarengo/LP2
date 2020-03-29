c2ded485deb619dac3427b41b70cfcd5d3140404
Gustavo Marengo <56846264+gumarengo@users.noreply.github.com>
2020-03-29 20:51:11 -0300

    Add files via upload

---
+++ PTesteClasses/PTesteClasses/frmMensalista.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace PTesteClasses
+{
+    partial class frmMensalista
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtMatricula = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtSalario = new System.Windows.Forms.TextBox();
+            this.txtData = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(48, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Matricula";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(48, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nome";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(48, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Salãrio Mensal";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(48, 102);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(129, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Data Entrada na Empresa";
+            // 
+            // txtMatricula
+            // 
+            this.txtMatricula.Location = new System.Drawing.Point(240, 29);
+            this.txtMatricula.Name = "txtMatricula";
+            this.txtMatricula.Size = new System.Drawing.Size(100, 20);
+            this.txtMatricula.TabIndex = 4;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(240, 51);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(225, 20);
+            this.txtNome.TabIndex = 5;
+            // 
+            // txtSalario
+            // 
+            this.txtSalario.Location = new System.Drawing.Point(240, 73);
+            this.txtSalario.Name = "txtSalario";
+            this.txtSalario.Size = new System.Drawing.Size(144, 20);
+            this.txtSalario.TabIndex = 6;
+            // 
+            // txtData
+            // 
+            this.txtData.Location = new System.Drawing.Point(240, 95);
+            this.txtData.Name = "txtData";
+            this.txtData.Size = new System.Drawing.Size(144, 20);
+            this.txtData.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(51, 143);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(168, 69);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Instanciar Mensalista";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(327, 143);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(168, 69);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Instanciar Mensalista passando parametros";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // frmMensalista
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(574, 261);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtData);
+            this.Controls.Add(this.txtSalario);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.txtMatricula);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmMensalista";
+            this.Text = "frmMensalista";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtMatricula;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtSalario;
+        private System.Windows.Forms.TextBox txtData;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ PTesteClasses/PTesteClasses/frmMensalista.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PTesteClasses
+{
+    public partial class frmMensalista : Form
+    {
+        public frmMensalista()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Mensalista mensalista = new Mensalista();
+            mensalista.NomeEmpregado = txtNome.Text;
+            mensalista.Matricula = Convert.ToInt32(txtMatricula.Text);
+            mensalista.DataEntradaEmpresa = Convert.ToDateTime(txtData.Text);
+            mensalista.SalarioMensal = Convert.ToDouble(txtSalario.Text);
+
+           MessageBox.Show("Nome=" + mensalista.NomeEmpregado + "\n" + "Matricula=" + mensalista.Matricula + 
+                "\n" + "Tempo Trabalho:" + mensalista.TempoTrabalho().ToString() + "\n" + "Salario final=" + 
+                mensalista.SalarioBruto().ToString("N2")); 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Mensalista mensalista = new Mensalista(Convert.ToInt32(txtMatricula.Text), txtNome.Text, Convert.ToDateTime(txtData.Text),Convert.ToDouble(txtSalario.Text));
+            MessageBox.Show("Nome=" + mensalista.NomeEmpregado + "\n" + "Matricula=" + mensalista.Matricula + 
+                "\n" + "Tempo Trabalho:" + mensalista.TempoTrabalho().ToString() + "\n" + "Salario final=" +
+                mensalista.SalarioBruto().ToString("N2")); 
+
+        }
+    }
+}
